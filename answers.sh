b6963005a514cc1c0f26e0fdae9b57c33f985ecc
JanaTartousy <tartousyjana@gmail.com>
2022-12-11 17:05:39 +0200

    Count & Filter

--- answers.sh
+++ answers.sh
@@ -1,3 +1,5 @@
+Basic Queries:
+
 select Name FROM students;
 SELECT AGE FROM STUDENTS WHERE AGE > 30;
 SELECT * FROM STUDENTS WHERE AGE='30' AND GENDER='F';
@@ -6,6 +8,9 @@ INSERT INTO STUDENTS(NAME, AGE, GENDER, POINTS) VALUES ('JANA', '20', 'F', '600'
 UPDATE STUDENTS SET POINTS=350 WHERE POINTS=300;
 UPDATE STUDENTS SET POINTS=150 WHERE POINTS=200;
 
+
+
+Creating Table:
 
 CREATE TABLE graduates (
     ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -19,6 +24,8 @@ INSERT INTO graduates (ID, Name, Age, Gender,Points)
  UPDATE graduates SET Graduation= "08/09/2018" WHERE Name="Layal";
  DELETE FROM students WHERE name ="Layal";
 
+
+ Joins:
  
 SELECT employees.Name, employees.Company, companies.date
 FROM employees, companies
@@ -29,3 +36,12 @@ FROM employees INNER JOIN companies ON employees.Company=companies.Name
  SELECT companies.name 
 FROM companies INNER JOIN employees ON employees.Company=companies.name
  WHERE employees.Role="Graphic Designer";
+
+
+Count & Filter:
+
+SELECT Name FROM STUDENTS WHERE Points=(SELECT max(points) FROM students);
+SELECT avg ("Points") FROM students;
+SELECT count(ID) FROM students WHERE Points="500";
+SELECT name FROM students WHERE name like "%s%";
+SELECT * FROM students ORDER BY Points DESC;
